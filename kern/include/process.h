f6297e2ffb621a5d223c322e6c139790ca838bd2
Potato Queue <potatoqueue@gmail.com>
2013-03-18 02:26:31 -0400

    FINISHED ASST2. FORKBOMB NOT PASSING

--- kern/include/process.h
+++ kern/include/process.h
@@ -6,15 +6,15 @@
 #define MAX_PROCESSES 10
 
 struct process {
-    int has_exited; // TODO init to 0
-    int exitcode; // TODO init to 0
-    pid_t parentpid; // TODO init on fork -> should we keep this?
-	pid_t pid;
-	struct cv* p_waitcv; // TODO malloc on create, free on destroy
-	struct lock* p_lock; // TODO malloc on create, free on destroy
-	struct thread* p_thread;
-    struct filetable* file_table; 
-    struct array * p_childrenpids; // TODO malloc on create, free on destroy
+    int has_exited; // 1 if this process has exited, 0 if not
+    int exitcode; // the exit code of the process
+    pid_t parentpid; // the pid of this process’s parent
+    pid_t pid; // the pid of this process
+    struct cv* p_waitcv; // condition variable in which to wait on
+    struct lock* p_lock; // lock to be used for synchronization during wait
+    struct thread* p_thread; // the thread that this process holds
+    struct filetable* file_table; // table of files currently opened in the process
+    struct array * p_childrenpids; // a list of all of this process’s children
 };
 
 // bootstraps initial process. calls thread bootstrap and processtable bootstrap
